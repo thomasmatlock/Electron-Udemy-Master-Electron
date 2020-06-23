8e18730dee53305138be4c5a5715096653da36c1
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2020-06-22 19:34:51 -0500

    123

--- lessons/8. Project/7. Deleting Items/7. Deleting Items/readItem.js
+++ lessons/8. Project/7. Deleting Items/7. Deleting Items/readItem.js
@@ -1,44 +1,40 @@
-
 // Modules
-const {BrowserWindow} = require('electron')
+const { BrowserWindow } = require("electron");
 
 // Offscreen BrowserWindow
-let offscreenWindow
+let offscreenWindow;
 
 // Exported readItem function
 module.exports = (url, callback) => {
+	// Create offscreen window
+	offscreenWindow = new BrowserWindow({
+		width: 500,
+		height: 500,
+		show: false,
+		webPreferences: {
+			offscreen: true,
+		},
+	});
 
-  // Create offscreen window
-  offscreenWindow = new BrowserWindow({
-    width: 500,
-    height: 500,
-    show: false,
-    webPreferences: {
-      offscreen: true
-    }
-  })
+	// Load item url
+	offscreenWindow.loadURL(url);
 
-  // Load item url
-  offscreenWindow.loadURL(url)
+	// Wait for content to finish loading
+	offscreenWindow.webContents.on("did-finish-load", (e) => {
+		// Get page title
+		let title = offscreenWindow.getTitle();
 
-  // Wait for content to finish loading
-  offscreenWindow.webContents.on('did-finish-load', e => {
+		// Get screenshot (thumbnail)
+		offscreenWindow.webContents.capturePage((image) => {
+			// Get image as dataURL
+			let screenshot = image.toDataURL();
 
-    // Get page title
-    let title = offscreenWindow.getTitle()
+			// Execute callback with new item object
+			callback({ title, screenshot, url });
 
-    // Get screenshot (thumbnail)
-    offscreenWindow.webContents.capturePage( image => {
-
-      // Get image as dataURL
-      let screenshot = image.toDataURL()
-
-      // Execute callback with new item object
-      callback({ title, screenshot, url })
-
-      // Clean up
-      offscreenWindow.close()
-      offscreenWindow = null
-    })
-  })
-}
+			// Clean up
+			offscreenWindow.close();
+			offscreenWindow = null;
+		});
+	});
+};
---
+++ notes.md
@@ -0,0 +1,18 @@
+# Text
+
+Section 1: Overview
+
+    - electron uses 2 processes:
+    -  the main process, which is a nodejs process
+    - the main nodejs process then creates instances of the chromium browser, sometimes multiple instances
+
+Section 2: Developing with Electron
+Section 3: Main Process API
+Section 4: Renderer Process API
+Section 5: IPC Communication
+Section 6: Shared API
+Section 7: Features & Techniques
+Section 8: Project
+Section 9: Application Distribution
+Section 10: MacOS Touch Bar
+Section 11: Outro
