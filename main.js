7d63475e476dd012c56574bf85f4e5165b53066b
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2020-08-06 21:00:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ function setupDisplays() {
 
 function createWindow() {
     console.log(devScreen.bounds.height);
-    devScreen.bounds.height < 1200 ?
+    devScreen.size.height < 1200 ?
         console.log('You are on the desktop') :
         console.log('You are on the laptop');
     const winDefaults = {
