b398452f9dd151c2b1dff394369d3035bcf0eebd
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2020-09-21 21:08:24 -0700

    idk

---
+++ push to library when done/notes-electron-builder.js
@@ -0,0 +1,6 @@
+// npm i -g electron-builder // install it globally
+// npm --help // for command line tools
+// npm i -d electron-builder // install locally to be run via npm
+// add script to package.json:         "build": "electron-builder --help" // https://www.electron.build/cli
+// to build solely for a specifc platform, flag with --{mac|linux|win}
+// appVeyor will build stuff for mac, windows will just build for win/linux
